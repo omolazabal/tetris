ea38770aae6379c78a6cc67828ca32e9ac203927
Oscar Olazabal <oscarolazabal@gmail.com>
2018-02-19 18:29:53 -0800

    add pygame controls

--- tetris.py
+++ tetris.py
@@ -1,52 +1,28 @@
 
-import pygame as pg
-import sys, termios, tty, os, time
+import pygame
+from pygame.locals import *
 import tetromino
 import grid
 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 GRID_HEIGHT = 20
 GRID_WIDTH = 10
+KEY_REPEAT_DELAY = 150
+KEY_REPEAT_INTERVAL = 30
 
+# Init Pygame
+pygame.init()
+pygame.key.set_repeat(KEY_REPEAT_DELAY, KEY_REPEAT_INTERVAL)
+game_display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Init Tetris
 tetris_board = grid.Grid(GRID_WIDTH, GRID_HEIGHT)
 shape = tetromino.Tetromino(GRID_WIDTH)
-
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-
 shape.new_shape()
-print(tetris_board.grid[3:, 3:GRID_WIDTH+3])
-# print(tetris_board.grid)
+quit = False
 
-while True:
-    char = getch()
-
-    if (char == "q"):
-        exit(0)
-
-    if (char == "r"):
-        shape.rotate()
-
-    if (char == "h"):
-        shape.move_left()
-
-    if (char == "j"):
-        shape.move_down()
-
-    if (char == "l"):
-        shape.move_right()
-
-    if (char == "n"):
-        shape.new_shape()
-
+def update():
     if tetris_board.update_grid(shape):
         if tetris_board.top_out_occured():
             tetris_board.new_grid()
@@ -59,5 +35,28 @@ while True:
     print(tetris_board.grid[3:, 3:GRID_WIDTH+3])
     # print(tetris_board.grid)
 
+while not quit:
+    keys_pressed = pygame.key.get_pressed()
 
+    for event in pygame.event.get():
+        if event.type == QUIT or keys_pressed[K_q]:
+            quit = True
+        elif keys_pressed[K_DOWN]:
+            shape.move_down()
+            update()
+        elif keys_pressed[K_LEFT]:
+            shape.move_left()
+            update()
+        elif keys_pressed[K_RIGHT]:
+            shape.move_right()
+            update()
+        elif keys_pressed[K_UP]:
+            shape.rotate()
+            update()
+        elif keys_pressed[K_n]:
+            shape.new_shape()
+            update()
+
+pygame.quit()
+exit(0)
 
