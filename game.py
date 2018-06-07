19c49396388a2dddaa4c5d145d3fc386464fe75c
Oscar Olazabal <oscarolazabal@gmail.com>
2018-06-06 22:29:28 -0500

    move score to core

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ def main():
     Tetris = tetris.Game()
 
     Tetris.set_grid_dim((10,20))
-    Tetris.set_screen_dim((640,480))
+    Tetris.set_screen_dim((1,1))
     Tetris.set_key_repeat_delay(150)
     Tetris.set_key_repeat_interval(30)
 
